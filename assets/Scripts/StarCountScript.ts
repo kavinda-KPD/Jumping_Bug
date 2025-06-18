bdabdaefb89b9f2a06849226579ab2ab60d8827b
kavinda-KPD <kavindadissanayake1999@gmail.com>
2025-06-18 15:36:40 +0530

    Add the Health Life count

--- assets/Scripts/StarCountScript.ts
+++ assets/Scripts/StarCountScript.ts
@@ -6,12 +6,17 @@ export class StarCountScript extends Component {
   @property(Label)
   starCountLabel: Label = null;
 
+  @property(Label)
+  HeathLifeCountLabel: Label = null;
+
   starCount: number = 0;
+  HeathLifeCount: number = 0;
 
   start() {}
 
   update(deltaTime: number) {
     this.calculateStarCount("add");
+    this.calculateHeathLifeCount("add");
   }
 
   calculateStarCount(method: "add" | "subtract") {
@@ -21,14 +26,27 @@ export class StarCountScript extends Component {
       this.starCount--;
     }
 
-    this.starCountLabel.string = "Stars: " + this.starCount.toString();
+    this.starCountLabel.string = "Stars ⭐ : " + this.starCount.toString();
+  }
+
+  calculateHeathLifeCount(method: "add" | "subtract") {
+    if (method === "add") {
+      this.HeathLifeCount++;
+    } else {
+      this.HeathLifeCount--;
+    }
+
+    this.HeathLifeCountLabel.string =
+      "Health🥷🏻: " + this.HeathLifeCount.toString();
   }
 
   showStarCountLabel() {
     this.starCountLabel.node.active = true;
+    this.HeathLifeCountLabel.node.active = true;
   }
 
   hideStarCountLabel() {
     this.starCountLabel.node.active = false;
+    this.HeathLifeCountLabel.node.active = false;
   }
 }
